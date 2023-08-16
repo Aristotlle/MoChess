c5cbf1d2adc9334f99777de065ee3555931027f8
Modestas Vievesis <Modestas.vievesis@gmail.com>
2023-08-16 22:03:07 +0300

    Rearranged directories

--- MainFile.py
+++ MainFile.py
@@ -6,14 +6,14 @@ class ChessBoard():
         self.root = Tk()
         self.root.geometry(f'{width}x{height}')
         self.chess_board = [
-            ['bR','bN','bB','bQ','bK','bB','bK','bR'],
+            ['bR','bN','bB','bQ','bK','bB','bN','bR'],
             ['bP','bP','bP','bP','bP','bP','bP','bP'],
             ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'],
             ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'],
             ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'],
             ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'],
             ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
-            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wK', 'wR']
+            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']
         ]
         self.selected_piece_position = None
         self.buttons = [[None for _ in range(8)] for _ in range(8)]
@@ -39,17 +39,55 @@ class ChessBoard():
 
     def button_clicked(self, row, col):
         if self.selected_piece_position is None:
+            if self.chess_board[row][col] == 'e': #tikrina ar nepaspaustas tuscias langelis.
+                return
             self.selected_piece_position = (row, col)
+            print("Button clicked:", row, col)
         else:
             target_row, target_col = row, col
             source_row, source_col = self.selected_piece_position
             moving_piece = self.chess_board[source_row][source_col]
-            self.chess_board[target_row][target_col] = moving_piece
-            self.chess_board[source_row][source_col] = 'e'
-            self.update_board()
+            if moving_piece != 'e':
+                if self.valid_move(moving_piece,source_row,source_col,target_row,target_col):
+                    self.chess_board[target_row][target_col] = moving_piece
+                    self.chess_board[source_row][source_col] = 'e'
+                    self.update_board()
 
             # Reset the selected piece position
             self.selected_piece_position = None
+
+    def valid_move(self,moving_piece, source_row, source_col, target_row, target_col):
+        print("Validating move:", moving_piece, source_row, source_col, target_row, target_col)
+        if moving_piece == 'wP':
+            return self.wpawn_movement(source_row,source_col,target_row,target_col)
+    def wpawn_movement(self, source_row, source_col, target_row, target_col):
+        print("Checking wpawn_movement:", source_row, source_col, target_row, target_col)
+        if target_col != source_col:
+            return False
+        if source_row == 6:
+            if target_row == source_row -2:
+                print("taip")
+                return True
+        if source_row == 6:
+            if target_row == source_row -1:
+                return True
+        if source_row != 6:
+            if target_row == source_row -1:
+                return True
+        else:
+            print("Ne")
+
+    # def king_movement(self):
+
+    # def queen_movement(self):
+    #
+    # def bishop_movement(self):
+    #
+    # def rook_movement(self):
+    #
+    # def knight_movement(self):
+
+
 
     def update_board(self):
         for row in range(8):
